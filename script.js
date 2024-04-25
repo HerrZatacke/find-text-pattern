ad8f3bb31c75014eef688f60178ab14b96b673e4
Andreas Hahn <zatacke@gmail.com>
2024-04-25 03:21:35 +0200

    add function to convert hex to text

--- script.js
+++ script.js
@@ -31,12 +31,19 @@ document.addEventListener('DOMContentLoaded', () => {
     }).join(' ');
   };
 
+  const reverse = (hex) => {
+    return hex.map((char) => {
+      return charMap.find(({code}) => char === code)?.value || `0x${char.toString(16).padStart(2, '0')}`
+    }).join('');
+  }
+
   input.addEventListener('keyup', update)
   input.addEventListener('blur', update)
   input.addEventListener('change', update)
 
   window.setTimeout(() => {
     init();
+    input.placeholder = reverse([0x9F, 0x5D, 0xA0, 0x68, 0x9D, 0x7E, 0x84, 0x64, 0x18, 0x86, 0x3F, 0x2F, 0x35, 0x2F, 0x32, 0x86, 0x19, 0x01, 0x4B, 0x12, 0x01, 0x1E, 0x0C, 0x05, 0x7C]);
     update();
   }, 200);
 })
